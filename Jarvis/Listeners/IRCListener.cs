875398f76aa7d52c467578b76e6f9c35914239b2
SwearWord <swearword@clossit.com>
2012-08-13 09:03:06 -0400

    More irc messing

--- Jarvis/Listeners/IRCListener.cs
+++ Jarvis/Listeners/IRCListener.cs
@@ -1,68 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
-using IrcDotNet;
+using Meebey.SmartIrc4net;
 
 namespace Jarvis.Listeners
 {
     public class IRCListener : ListenerBase
     {
         private IrcClient _client;
+
         public IRCListener(Pipe pipe)
             : base(pipe)
         {
-            _client = new IrcClient();
-            _client.FloodPreventer = new IrcStandardFloodPreventer(4, 2000);
         }
 
         public override void Loop()
         {
-            _client = new IrcClient();
-            _client.Registered +=
-                (sender, args) =>
-                {
-
-                    _client.LocalUser.JoinedChannel += (sender1, args1) =>
-                        {
-                            args1.Channel.MessageReceived += (o, eventArgs) => Handle(eventArgs.Text);
-                            RawOutput("I have joined " + args1.Channel.Name);
-                        };
-                    _client.Channels.Join("#jarvis");
-                    Brain.ListenerManager.CurrentListener.Output("I have made a connection to Rizon.");
-                };
-            _client.Disconnected += (sender, args) =>
+            while (true)
             {
-                Brain.ListenerManager.CurrentListener.Output("Disconnected from Rizon. I shall try reconnecting in 5 seconds.");
-                5.Seconds().Sleep();
-                Loop();
-            };
 
-            _client.Error += (sender, args) =>
-            {
-                Console.WriteLine(args.Error.Message);
-                _client.Disconnect();
-            };
-            _client.Connect("irc.rizon.net", 6667, false, new IrcUserRegistrationInfo()
+                _client = new IrcClient
+                    {
+                        AutoJoinOnInvite = true,
+                        AutoReconnect = true,
+                        AutoRejoin = true,
+                        AutoRejoinOnKick = true,
+                        AutoRelogin = true,
+                        AutoRetry = true
+                    };
+                var mre = new ManualResetEvent(false);
+                _client.OnJoin += (sender, args) =>
+                                  Output("I have joined " + args.Channel);
+                _client.OnChannelMessage += (sender, args) =>
+                                            Handle(args.Data.Message);
+                _client.OnConnected += (sender, args) =>
+                    {
+                        mre.Set();
+                        _client.RfcJoin("#jarvis");
+                    };
+                _client.OnError += (sender, args) =>
+                    {
+                        _client.Disconnect();
+                        Loop();
+                    };
+
+                _client.Connect("irc.clossit.com", 6668);
+                _client.Login("Jarvis", "Jarvis");
+                new Thread(() => _client.Listen()).Start();
+                mre.WaitOne(30.Seconds());
+                while (_client.IsConnected)
                 {
-                    NickName = "Jarvis",
-                    UserName = "Jarvis",
-                    RealName = "Jarvis"
-                });
+                    5.Seconds().Sleep();
+                }
+                Output("I have lost connection to I.R.C");
+            }
         }
 
         public override void RawOutput(string output)
         {
-            if (_client.IsRegistered)
+            if (true)
             {
                 var lines = output.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
                 foreach (var line in lines)
                 {
-                    foreach (var channel in _client.Channels)
+                    foreach (var channel in _client.JoinedChannels)
                     {
-                        _client.LocalUser.SendMessage(channel, line);
+                        _client.SendMessage(SendType.Message, channel, line);
                     }
                 }
             }
